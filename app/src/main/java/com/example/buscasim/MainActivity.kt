e05058c31f86111915f30be44fa2095bf7bffdcd
MatheusHenrick <matheu81h@gmail.com>
2023-09-24 22:06:06 -0300

    Adicionando restrições para o desenho do grafo e eventos para os nós

--- app/src/main/java/com/example/buscasim/MainActivity.kt
+++ app/src/main/java/com/example/buscasim/MainActivity.kt
@@ -25,22 +25,157 @@ class MainActivity : AppCompatActivity() {
         binding2 = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var numeroDigitado: Int = -1
+        var emExec: Boolean = false
         val g = Graph(15)
 
-        g.addEdge(0, 1)
-        g.addEdge(0, 4)
-        g.addEdge(1, 2)
-        g.addEdge(1, 3)
-        g.addEdge(1, 4)
-        g.addEdge(2, 3)
-        g.addEdge(3, 4)
+        g.addEdge(0, 9)
+        g.addEdge(0, 13)
+        g.addEdge(1, 7)
+        g.addEdge(1, 8)
+        g.addEdge(1, 9)
+        g.addEdge(2, 9)
+        g.addEdge(3, 6)
+        g.addEdge(3, 8)
+        g.addEdge(3, 13)
+        g.addEdge(3, 14)
+        g.addEdge(4, 6)
+        g.addEdge(4, 11)
+        g.addEdge(5, 8)
+        g.addEdge(6, 3)
+        g.addEdge(6, 4)
+        g.addEdge(6, 11)
+        g.addEdge(7, 1)
+        g.addEdge(8, 3)
+        g.addEdge(8, 5)
+        g.addEdge(8, 12)
+        g.addEdge(8, 14)
+        g.addEdge(9, 0)
+        g.addEdge(9, 1)
+        g.addEdge(9, 2)
+        g.addEdge(9, 10)
+        g.addEdge(10, 9)
+        g.addEdge(11, 6)
+        g.addEdge(11, 4)
+        g.addEdge(12, 8)
+        g.addEdge(13, 0)
+        g.addEdge(13, 3)
+        g.addEdge(14, 3)
+        g.addEdge(14, 8)
 
         println("Breadth-First Search:")
-        g.BFS(0)
 
         binding.buttonComecar.setOnClickListener {
             setContentView(binding2.root)
         }
+
+        binding2.button0.setOnClickListener {
+            numeroDigitado = 0
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button1.setOnClickListener {
+            numeroDigitado = 1
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button2.setOnClickListener {
+            numeroDigitado = 2
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button3.setOnClickListener {
+            numeroDigitado = 3
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button4.setOnClickListener {
+            numeroDigitado = 4
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button5.setOnClickListener {
+            numeroDigitado = 5
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button6.setOnClickListener {
+            numeroDigitado = 6
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button7.setOnClickListener {
+            numeroDigitado = 7
+            if (!emExec) {
+                binding2.textViewFilaElementos.text = numeroDigitado.toString()
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button8.setOnClickListener {
+            numeroDigitado = 8
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button9.setOnClickListener {
+            numeroDigitado = 9
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button10.setOnClickListener {
+            numeroDigitado = 10
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button11.setOnClickListener {
+            numeroDigitado = 11
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button12.setOnClickListener {
+            numeroDigitado = 12
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button13.setOnClickListener {
+            numeroDigitado = 13
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+        binding2.button14.setOnClickListener {
+            numeroDigitado = 14
+            if (!emExec) {
+                emExec = true
+                g.BFS(numeroDigitado)
+            }
+        }
+
     }
 }
 
@@ -63,6 +198,7 @@ class Graph(private val numVertices: Int) {
 
         visited[startVertex] = true
         queue.add(startVertex)
+        //binding2.textViewFilaElementos.text = numeroDigitado.toString()
 
         while (queue.isNotEmpty()) {
             val vertex = queue.poll()
